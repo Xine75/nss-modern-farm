c72e85168f6e2186bffbec97f84f8987adc6b222
Xine75 <cjdoza@gmail.com>
2020-11-20 11:04:03 -0600

    created seeds

---
+++ src/scripts/main.js
@@ -0,0 +1,51 @@
+console.log("Welcome to the main module")
+
+import { createPlan } from "./plan.js"
+createPlan();
+
+const yearlyPlan = createPlan();
+
+console.log(yearlyPlan);
+
+// testing asparagus
+
+import { createAsparagus } from "./seeds/asparagus.js"
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed)
+
+// testing corn
+
+import { createCorn } from "./seeds/corn.js"
+
+const cornSeed = createCorn()
+console.log(cornSeed)
+
+// testing potato
+
+import { createPotato } from "./seeds/potato.js"
+
+const potatoSeed = createPotato()
+console.log(potatoSeed)
+
+// testing soybean
+
+import { createSoybean } from "./seeds/soybean.js"
+
+const soybeanSeed = createSoybean()
+console.log(soybeanSeed)
+
+// testing sunflower
+
+import { createSunflower } from "./seeds/sunflower.js"
+
+const sunflowerSeed = createSunflower()
+console.log(sunflowerSeed)
+
+// testing wheat
+
+import { createWheat } from "./seeds/wheat.js"
+
+const wheatSeed = createWheat()
+console.log(wheatSeed)
+
