a985f15edf9a99890e95f07823e77e993c9f18f8
Xine75 <cjdoza@gmail.com>
2020-12-02 16:07:47 -0600

    added catalog but not working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,20 +2,22 @@ import { createPlan } from "./plan.js"
 import { usePlants } from "./field.js"
 import { plantSeeds } from "./tractor.js"
 import { harvestPlants} from "./harvester.js"
-
-
+import { Catalog } from "./catalog.js"
 
 const yearlyPlan = createPlan();
 
 plantSeeds(yearlyPlan);
 console.log(usePlants())
-
 const plantsToHarvest = usePlants();
 
 const harvest = harvestPlants(plantsToHarvest)
 console.log("Harvester", harvest)
 
 console.log("Yearly Plan!", yearlyPlan);
+
+
+testing = Catalog()
+console.log(testing)
 
 // // testing asparagus
 
